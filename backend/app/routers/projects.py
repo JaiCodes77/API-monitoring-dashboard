4ff38f646b39c33bedefddfad5d2269e1f6ed881
Jai Pandey <jaipandeyzerozero@gmail.com>
2026-02-16 00:12:54 +0530

    created a post route for project creation

--- backend/app/routers/projects.py
+++ backend/app/routers/projects.py
@@ -6,4 +6,21 @@ from app.models.project import Project
 from app.models.user import User
 from app.schemas.project import ProjectCreate, ProjectOut, ProjectUpdate
 
-router = APIRouter(prefix="/projects", tags=["projects"])
+router = APIRouter(prefix="/projects", tags=["projects"]) 
+
+
+@router.post('/', response_model=ProjectOut, status_code=status.HTTP_201_CREATED)
+def create_project(
+payload : ProjectCreate,
+owner_id : int = Query(...),
+db : Session = Depends(get_db)
+):
+    owner = db.get(User, owner_id) 
+    if not owner: 
+        raise HTTPException(status_code=404, detail="owner user not found!")
+    
+    project = Project(name = payload.name, owner_id = owner_id)
+    db.add(project)
+    db.commit()
+    db.refresh(project)
+    return project
